96bfa52ca5b4160c66a366416d46b50addb4578d
JC <jc.jiechen.jane@gmail.com>
2024-02-08 21:47:37 -0500

    temp

---
+++ src/Labs/a3/JavaScript/functions/WorkingWithFunctions.tsx
@@ -0,0 +1,15 @@
+import ES5Functions from "./ES5Functions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+
+function WorkingWIthFunctions() {
+  return (
+    <div>
+      <ES5Functions />
+      <ArrowFunctions />
+      <ImpliedReturn />
+    </div>
+  );
+}
+
+export default WorkingWIthFunctions;
---
+++ src/Labs/a3/index.tsx
@@ -0,0 +1,11 @@
+import JavaScript from "./JavaScript/JavaScript";
+
+function Assignment3() {
+  return (
+    <div className="container">
+      <h1>Assignment 3</h1>
+      <JavaScript />
+    </div>
+  );
+}
+export default Assignment3;
