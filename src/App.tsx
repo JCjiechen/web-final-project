c94c999ac236d80ca0cbf34108667c5bb6eb75f3
JC <jc.jiechen.jane@gmail.com>
2024-02-18 20:54:26 -0500

    a3-labs-router

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Labs from "./Labs";
 import HelloWorld from "./Labs/a3/HelloWorld";
 import Kanbas from "./Kanbas";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Labs />
-      <Kanbas />
-      <HelloWorld />
-    </div>
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="/Labs" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+          <Route path="/hello" element={<HelloWorld />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,9 +1,17 @@
+import { Link, Route, Routes } from "react-router-dom";
+import Nav from "../Nav";
 import Assignment3 from "./a3";
 
 function Labs() {
   return (
-    <div>
-      <Assignment3 />
+    <div className="container-fluid">
+      <Nav />
+      <h1>Labs</h1>
+      <Link to="/Labs/a3">Assignment 3</Link> |{" "}
+      <Link to="/Labs/a4">Assignment 4</Link>
+      <Routes>
+        <Route path="/a3/*" element={<Assignment3 />} />
+      </Routes>
     </div>
   );
 }
