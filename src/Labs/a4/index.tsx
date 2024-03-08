8bcbc7e21bd9a6494bdd60587bf2d68617a48d68
JC <jc.jiechen.jane@gmail.com>
2024-03-07 21:56:31 -0500

    a4-Lab-handling user events

---
+++ src/Labs/a4/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ClickEvent from "./ClickEvent";
+import EventObject from "./EventObject";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+
+const Assignment4 = () => {
+  function sayHello() {
+    alert("Hello");
+  }
+
+  return (
+    <>
+      <h1>Assignment 4</h1>
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject />
+    </>
+  );
+};
+
+export default Assignment4;
