4b0056c701bc9d0214caee7c72c6b36931350ce5
JC <jc.jiechen.jane@gmail.com>
2024-03-27 17:46:50 -0500

    a5 Lab HTTP Request

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,6 +2,7 @@ import { Link, Navigate, Route, Routes } from "react-router-dom";
 import Nav from "../Nav";
 import Assignment3 from "./a3";
 import Assignment4 from "./a4";
+import Assignment5 from "./a5";
 import store from "./store";
 import { Provider } from "react-redux";
 
@@ -14,6 +15,7 @@ function Labs() {
           <Route path="/" element={<Navigate to="/Labs/a3" />} />
           <Route path="/a3/*" element={<Assignment3 />} />
           <Route path="/a4/*" element={<Assignment4 />} />
+          <Route path="/a5/*" element={<Assignment5 />} />
         </Routes>
       </div>
     </Provider>
